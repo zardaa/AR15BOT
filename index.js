ce28a2cde93c07d479bc18b71f1b6e27a343d0f5
A187ID <73683578+A187ID@users.noreply.github.com>
2020-11-24 00:53:41 +0700

    Penambahan fitur baru ;)

--- index.js
+++ index.js
@@ -806,6 +806,35 @@ if (text.includes("$fotoanime"))
     });
     }
 
+if (text.includes("$hentai"))
+   {
+    var items = ["nsfwneko","anime hentai"];
+    var anim = items[Math.floor(Math.random() * items.length)];
+    var url = "https://api.computerfreaker.cf/v1/";
+    
+    axios.get(url)
+      .then((result) => {
+        var b = JSON.parse(JSON.stringify(result.data));
+        var anim =  b[Math.floor(Math.random() * b.length)];
+        imageToBase64(anim) // Path to the image
+        .then(
+            (response) => {
+	var buf = Buffer.from(response, 'base64'); // Ta-da	
+              conn.sendMessage(
+            id,
+              buf,MessageType.image)
+       
+            }
+        )
+        .catch(
+            (error) => {
+                console.log(error); // Logs an error if there was one
+            }
+        )
+    
+    });
+    }
+
 if (text.includes("$loli"))
    {
     var items = ["anime loli","anime loli sange","anime loli fackgirll","anime loli i love you"];
