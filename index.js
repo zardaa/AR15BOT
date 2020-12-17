076fd9acacdede08a440c016a0460e457cb7fcc4
A187ID <73683578+A187ID@users.noreply.github.com>
2020-12-17 12:05:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -165,7 +165,8 @@ client.on('message-new', async (m) => {
    const prefix = messageType === 'imageMessage' ? imageMessage.caption.split(' ')[0].split(re)[1] : text.split(' ')[0].split(re)[1] // multiple prefix
 
    console.log(`[ ${time} ] => Nomor: [ ${id.split("@s.whatsapp.net")[0]} ] => ${text}`); 
-       case 'P':
+switch 
+	case 'P':
        case 'p':
        case 'Bro':
        case 'Bot':
